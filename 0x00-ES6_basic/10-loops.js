f5c4a0242bed5487b3d2759e83f7964c6a8dd6a9
mdallen5393 <allen_matthew@outlook.com>
2023-01-06 15:46:05 -0600

    Task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     array[array.indexOf(value)] = appendString + value;
