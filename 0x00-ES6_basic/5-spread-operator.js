0ae0c3a3ae0f014ebabee229180d2694cbb53cfc
mdallen5393 <allen_matthew@outlook.com>
2023-01-06 14:01:22 -0600

    Task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,5 @@
 export default function concatArrays(array1, array2, string) {
-  return array1.concat(...array2, ...string);
+  const arr = [];
+  arr.push(...array1, ...array2, ...string);
+  return arr;
 }
