b0cc66f2dc31f83071a8d7ab7f9f135b90c8066e
mdallen5393 <allen_matthew@outlook.com>
2023-01-06 15:08:15 -0600

    Task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   return {
     ...budget,
-    getIncomeInDollars: (income) => {
-      return `$${income}`;
-    },
-    getIncomeInEuros: (income) => {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: (income) => { return `$${income}` },
+    getIncomeInEuros: (income) => { return `${income} euros` },
   };
 }
