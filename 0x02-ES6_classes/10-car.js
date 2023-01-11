e0d6a13e210a00864d95c2d9f4fe02ffed645d46
mdallen5393 <allen_matthew@outlook.com>
2023-01-11 14:41:02 -0600

    Task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,6 @@ export default class Car {
   }
 
   cloneCar() {
-    return Object.assign(Object.create(Object.getPrototypeOf(this)),
-    {_brand:undefined, _motor:undefined, _color:undefined});
+    return new this.constructor;
   }
 }
