ad581060bfee282fe43d9642426b188b236dba4b
mdallen5393 <allen_matthew@outlook.com>
2023-01-18 11:28:08 -0600

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,9 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city)
     .map((student) => {
       for (const field of newGrades) {
+        student.grade = 'N/A';
         if (student.id === field.id) {
           student.grade = newGrades.grade;
-        } else {
-          student.grade = 'N/A';
         }
       }
     }
