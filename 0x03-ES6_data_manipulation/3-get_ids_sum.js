17bd2063f343baf41aa4e49a361790d432082786
mdallen5393 <allen_matthew@outlook.com>
2023-01-18 10:48:17 -0600

    Task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,6 @@
+import getListStudentIds from './1-get_list_student_ids';
+
 export default function getStudentIdsSum(students) {
-  let sum = 0;
-  return students.reduce((sum, curr) => sum + students.id);
+  const ids = getListStudentIds(students);
+  return students.reduce((sum, curr) => sum + curr);
 }
