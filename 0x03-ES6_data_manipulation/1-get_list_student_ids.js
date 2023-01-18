0df38519b569c1fa67a652f4834bbbf82dceb8d0
mdallen5393 <allen_matthew@outlook.com>
2023-01-18 10:27:57 -0600

    Task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,5 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  const students_map = new Map(students);
-  return [...students_map.get('id')];
+  return students.map(id => students['id']);
 }
