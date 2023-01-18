7163118649807816d4d92fe40713448fea839eaf
mdallen5393 <allen_matthew@outlook.com>
2023-01-18 11:57:49 -0600

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const int8 = new Int8Array(length);
-  int8[position] = value;
-  return int8;
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  view[position] = value;
+  return buffer;
 }
