293c664c1caca97028616c3c944937e13aa2496a
mdallen5393 <allen_matthew@outlook.com>
2023-05-08 14:18:11 -0500

    Task 1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function `concat` that takes a string `str1`
+and a string `str2` as arguments and returns a concatenated
+string.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenates two strings and returns the
+    new string.
+    """
+    return (str1 + str2)
