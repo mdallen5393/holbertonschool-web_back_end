c7a87865a9a4495a97013e7fb4234a3e15eab0e5
mdallen5393 <allen_matthew@outlook.com>
2023-05-08 19:35:31 -0500

    Task 9. Lets duck type an iterable object

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """
-Function that takes in a list of integers and returns a
-tuple containing each list value and the length of each
-list value
+Function that does something with someone that defies my
+understanding entirely.
 """
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Function that takes a list and returns a tuple with
-    each list item and the items length.
+    Magic.
     """
     return [(i, len(i)) for i in lst]
