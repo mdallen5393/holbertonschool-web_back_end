913d9de1a79a8b08f9dc2615596e1d05da6ee9ff
mdallen5393 <allen_matthew@outlook.com>
2023-05-15 19:35:12 -0500

    Task 0. Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,11 +7,20 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    Implementation of a cache using a basic dictionary.
+    """
     def put(self, key, item):
+        """
+        Adds an item to the cache.
+        """
         if (key and item):
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Retrieves an item from the cache.
+        """
         if not key or key not in self.cache_data:
             return None
         return self.cache_data[key]
