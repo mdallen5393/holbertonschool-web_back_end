a8adf1abc6bdc5499daa347420a3d5ab5d52b971
mdallen5393 <allen_matthew@outlook.com>
2023-05-23 09:34:33 -0500

    Task 6. Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -20,5 +20,7 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     Validates whether the provided password matches the
     hashed password.
     """
-    if bcrypt.checkpw(password, hashed_password):
-        return
+    encoded_password = password.encode('utf-8')
+    if bcrypt.checkpw(encoded_password, hashed_password):
+        return True
+    return False
