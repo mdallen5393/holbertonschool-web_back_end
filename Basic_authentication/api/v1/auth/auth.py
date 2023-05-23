12e8897602648d661bca664fe16ac98e7e5cde91
mdallen5393 <allen_matthew@outlook.com>
2023-05-23 14:27:40 -0500

    Task 3. Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module containing basic API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def __init__(self):
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns None
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User object
+        """
+        return None
