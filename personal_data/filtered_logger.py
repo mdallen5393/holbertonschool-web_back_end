d24903e3551f5c000a9913c3395df294a8a39282
mdallen5393 <allen_matthew@outlook.com>
2023-05-22 19:03:09 -0500

    Task 0. Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,8 +8,9 @@ from typing import List
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ 
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
