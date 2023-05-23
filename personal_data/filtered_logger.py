49c0a8d27d4b06bb52f9bec8f5aeefbebb718343
mdallen5393 <allen_matthew@outlook.com>
2023-05-22 20:13:47 -0500

    Task 2. Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import logging
 import re
 from typing import List
 
-PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
