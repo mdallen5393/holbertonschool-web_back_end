2edc455878a6817bfe38601137f872c1f2b59105
mdallen5393 <allen_matthew@outlook.com>
2023-05-25 08:04:07 -0500

    Task 6. Basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -19,6 +19,10 @@ if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains the BasicAuth class."""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class that provides basic authentication"""
+    pass
