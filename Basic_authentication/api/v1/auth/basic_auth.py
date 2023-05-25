653cc7d45cac687290139a9755a5ff1d84d7aed8
mdallen5393 <allen_matthew@outlook.com>
2023-05-25 10:12:38 -0500

    Task 10. Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,13 +58,13 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({'email': user_email})
-        if len(users) == 0:
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
             return None
 
-        user = users[0]
-        if not user.is_valid_password(user_pwd):
-            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
 
-        return user
-
+        return None
