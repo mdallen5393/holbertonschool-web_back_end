c0e9d632cc82dbf9f102ce4d97190efc63ec88fe
mdallen5393 <allen_matthew@outlook.com>
2023-05-26 14:00:21 -0500

    Task 4. Session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -34,3 +35,9 @@ class Auth:
         """Returns a User object
         """
         return None
+    
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv('SESSION_NAME'))
