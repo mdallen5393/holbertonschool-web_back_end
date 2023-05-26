b417dd703295f9023c61f62995d98fc46f253d81
mdallen5393 <allen_matthew@outlook.com>
2023-05-26 14:14:02 -0500

    Task 5. Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ def before_request():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        if not auth.authorization_header(request) and not auth.session_cookie(request):
+        if auth.authorization_header(request) == None and auth.session_cookie(request) == None:
             abort(401)
         request.current_user = auth.current_user(request)
 
