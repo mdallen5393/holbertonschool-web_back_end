52edfc2a08bc03358abc52977fb9bb4fcebebfbf
mdallen5393 <allen_matthew@outlook.com>
2023-05-26 13:40:23 -0500

    Task 2. Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Module that contains SessionAuth class definition."""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Allows 'in-memory' Session ID storing"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a `user_id`"""
+        if user_id is None or isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
