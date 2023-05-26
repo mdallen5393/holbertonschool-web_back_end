8d9c4bd38fe8921c92797a570b60682373ddf0a7
mdallen5393 <allen_matthew@outlook.com>
2023-05-26 13:41:55 -0500

    Task 2. Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,7 +10,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a `user_id`"""
-        if user_id is None or isinstance(user_id, str):
+        if user_id is None or type(user_id) != str:
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
