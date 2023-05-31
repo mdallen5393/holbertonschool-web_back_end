5acbdb7c611dff222244ee9207397edfedc9c4f4
mdallen5393 <allen_matthew@outlook.com>
2023-05-31 01:34:09 -0500

    Task 15. User profile

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -73,7 +73,7 @@ def profile():
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
-    return jsonify({"email", f"{user.email}"}), 200
+    return jsonify({"email": f"{user.email}"}), 200
 
 
 if __name__ == "__main__":
