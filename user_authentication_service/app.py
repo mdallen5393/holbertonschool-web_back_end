389a7f7dbdca76d2bfb0d4ff97b55aad4ab330c2
mdallen5393 <allen_matthew@outlook.com>
2023-05-31 01:01:08 -0500

    Task 11. Log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Contains definition for a Basic Flask app"""
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 
 app = Flask(__name__)
@@ -29,6 +29,25 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except ValueError:
         return {"message": "email already registered"}, 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
