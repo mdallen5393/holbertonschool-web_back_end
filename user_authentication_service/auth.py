3f1a1be6a248306199396578441f8f525a82be17
mdallen5393 <allen_matthew@outlook.com>
2023-05-30 23:02:45 -0500

    Task 5. Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module that contains the Auth class"""
+from user import User
 from db import DB
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -10,6 +12,18 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Hashes the password and saves the user to the database"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user:
+            raise ValueError(f"User {email} already exists.")
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
 
 
 def _hash_password(password: str) -> bytes:
