aeb732531ee6bbd27ce1a1985336741e0b0407d1
mdallen5393 <allen_matthew@outlook.com>
2023-05-30 23:13:45 -0500

    Task 6. Basic Flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,5 +1,21 @@
+#!/user/bin/env python3
+"""Contains definition for a Basic Flask app"""
 from auth import Auth
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """sets up basic route"""
+    return jsonify({"message": "Bienvenue"}), 200
 
 
 AUTH = Auth()
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0, port="5000")
