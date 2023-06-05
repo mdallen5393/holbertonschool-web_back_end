3dbc59b64a30874f63d0e49eae8a494f9a34ee04
mdallen5393 <allen_matthew@outlook.com>
2023-06-05 18:44:50 -0500

    Task 1. Basic Babel setup

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 """Simple flask app with index.html template"""
 from flask import Flask, render_template
-from flask_babel import Babel
-
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config():
-    """Class which configures available languages"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 @app.route('/')
