2466e5d1c19ebfd4020ffc2757b00e89cd144984
mdallen5393 <allen_matthew@outlook.com>
2023-06-05 12:50:34 -0500

    Task 5. mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,17 @@ class TestGithubOrgClient(unittest.TestCase):
         test_client.org()
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    @patch('client.GithubOrgClient.org')
+    def test_public_repose_url(self, mock_org):
+        """
+        Unit-tests the _public_repos_url property of the GithubOrgClient class
+        """
+        test_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = test_payload
+
+        test_client = GithubOrgClient("google")
+        result = test_client._public_repos_url
+
+        self.assertEqual(result, test_payload["repos_url"])
+
