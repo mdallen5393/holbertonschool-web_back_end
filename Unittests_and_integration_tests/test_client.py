55efc71ca5f3d814e51b38bd67c05baa0d371996
mdallen5393 <allen_matthew@outlook.com>
2023-06-05 12:27:34 -0500

    Task 4. Parameterize and patch as decorators

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Test Suite for testing client.py"""
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests the GithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Tests whether GithubOrgClient.org returns the correct value"""
+        test_client = GithubOrgClient(org_name)
+        test_client.ort()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
