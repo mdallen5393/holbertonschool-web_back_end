a7e9f8a968cfe80c53a5a795cf34c4e280da9cdd
mdallen5393 <allen_matthew@outlook.com>
2023-06-05 18:53:38 -0500

    Task 2. Get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -23,7 +23,7 @@ def index():
     return render_template('0-index.html')
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """Retrieves locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
