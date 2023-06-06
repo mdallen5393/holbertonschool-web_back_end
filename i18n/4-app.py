ae11285d94d3ffc645d6bb3aa865519766a94a28
mdallen5393 <allen_matthew@outlook.com>
2023-06-05 20:19:57 -0500

    Task 4. Force locale with URL parameter

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,76 +1,36 @@
 #!/usr/bin/env python3
-""" Flask App """
-
-
+"""Simple flask app with index.html template"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-babel = Babel(app)
 
 
-class Config:
-    """ Config app class """
+class Config():
+    """Class which configures available languages"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route for `/`"""
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ get locale """
+    """Retrieves locale from request"""
     locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """ Index page """
-    return render_template('4-index.html')
-
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, threaded=True, debug=True)
-
-# #!/usr/bin/env python3
-# """Simple flask app with index.html template"""
-# from flask import Flask, render_template, request
-# from flask_babel import Babel
-
-# app = Flask(__name__)
-
-
-# class Config():
-#     """Class which configures available languages"""
-#     LANGUAGES = ["en", "fr"]
-#     BABEL_DEFAULT_LOCALE = 'en'
-#     BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# app.config.from_object(Config)
-# babel = Babel(app)
-
-
-# @app.route('/', strict_slashes=False)
-# def index():
-#     """Route for `/`"""
-#     return render_template('4-index.html')
-
-
-# @babel.localeselector
-# def get_locale():
-#     """Retrieves locale from request"""
-#     locale = request.args.get('locale')
-#     if locale and locale in Config.LANGUAGES:
-#         return locale
-#     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-# if __name__ == '__main__':
-#     app.run()
+    app.run()
