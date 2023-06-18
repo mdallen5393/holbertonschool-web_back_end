b63e51d29817f34e19b2e9c783bf6667cd4a661a
mdallen5393 <allen_matthew@outlook.com>
2023-06-18 16:26:28 -0500

    Task 3. Old school band

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with `Glam rock` as
 -- their main style, ranked by their longevity.
-SELECT band_name, IFNULL((split - formed), YEAR(CURDATE()) - formed) AS longevity
+SELECT band_name, IFNULL((split - formed), 2020 - formed) AS longevity
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY longevity DESC;
