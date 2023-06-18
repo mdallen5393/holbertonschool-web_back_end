deaa1c7a647b743a8e7d0b24f9e29f5850fdc0fb
mdallen5393 <allen_matthew@outlook.com>
2023-06-18 16:52:54 -0500

    Task 4. Buy buy buy

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,14 +1,10 @@
 -- SQL script that creates a trigger that decreases the
 -- quantity of an item after adding a new order.
-DELIMITER //
-
 CREATE TRIGGER decrease_item
-AFTER UPDATE ON orders
+AFTER INSERT ON orders
 FOR EACH Row
 BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = New.item_name;
-END //
-
-DELIMITER ;
+END;
