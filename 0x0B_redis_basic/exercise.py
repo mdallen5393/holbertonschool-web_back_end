890fac2c3408c43345bdb4dc325c2ec6e9176b9c
mdallen5393 <allen_matthew@outlook.com>
2023-06-20 17:33:51 -0500

    Added 1-main.py for testing; Task 1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module which defines the Cache class using Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 class Cache:
     """Defines a Redis cache"""
@@ -16,3 +16,17 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        value = self._redis.get(key)
+        if value is not None and fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        value = self.get(key, str)
+        return value
+
+    def get_int(self, key: str) -> Optional[int]:
+        value = self.get(key, int)
+        return value
