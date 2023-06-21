d148240b74f0305efd40806423aca06d02502d5a
mdallen5393 <allen_matthew@outlook.com>
2023-06-21 13:55:07 -0700

    Task 12 test

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
-"""
-Script that provides some stats about Nginx logs stored
-in MongoDB.
-"""
+""" Provides some stats about Nginx logs stored in MongoDB """
+
+
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:2717/')
-    logs_collection = client.logs.nginx
-
-    # Get number of docs in the collection
-    num_logs = logs_collection.count_documents({})
-    print(f"{num_logs} logs")
-
-    # Get number of docs for each method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def loggedStats():
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient()
+    db = client.logs
+    logs = db.nginx
+    print("{} logs".format(logs.count_documents({})))
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = logs_collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {count}")
+        print(f"\tmethod {method}: " +
+              f"{logs.count_documents({'method': method})}")
+    print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})} \
+status check")
 
-    # Get num docs with method=GET and path=/status
-    count = logs_collection.count_documents(
-        {'method': 'GET', 'path': '/status'})
-    print(f"{count} status check")
+
+if __name__ == "__main__":
+    loggedStats()
