a70807e63e1e967ce9a0b38e1c93f87a1549b00c
mdallen5393 <allen_matthew@outlook.com>
2023-06-21 13:18:14 -0700

    Task 10. Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -10,7 +10,7 @@ def update_topics(mongo_collection, name, topics):
     Changes all topics of a school document based on the
     name.
     """
-    mongo_collection.update(
+    mongo_collection.updateMany(
         {"name": name},
         {"$set": {'topics': topics}}
     )
