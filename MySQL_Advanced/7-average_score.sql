f4b85de2dd378227b353e1b17fa7b2646d137254
mdallen5393 <allen_matthew@outlook.com>
2023-06-22 10:15:48 -0500

    Task 7. Average score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END;
 //
