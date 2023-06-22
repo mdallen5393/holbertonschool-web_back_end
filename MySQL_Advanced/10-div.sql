53e7b931557e47d7c7f4464a107d2ad474ee961f
mdallen5393 <allen_matthew@outlook.com>
2023-06-22 11:24:56 -0500

    Task 10. Safe divide

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- SQL script that creates a function `SaveDiv` that
+-- divides (and returns) the first by the second number
+-- or returns 0 if the second number is equal to 0.
+DELIMITER //
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END;
+//
+
+DELIMITER ;
