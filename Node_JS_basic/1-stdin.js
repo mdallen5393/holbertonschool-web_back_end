ef445b7646f323ad8b41cfb96e4dffeb918feb75
mdallen5393 <allen_matthew@outlook.com>
2023-06-26 19:31:58 -0500

    Task 1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,12 +4,12 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name) {
-      process.stdout.write(`Your name is: ${name}`);
-    }
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
