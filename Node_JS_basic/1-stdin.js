14383b62dfce735fdbb51394af250c7fa6525459
mdallen5393 <allen_matthew@outlook.com>
2023-06-26 19:02:18 -0500

    Task 1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,15 @@
 // Program that displays a string, takes inputs, and
 // responds
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+console.log('Welcome to Holberton School, what is your name?');
 
-readline.question('Welcome to Holberton School, what is your name?\n', name => {
-  if (name.trim() === '') {
-    process.stdout.write('No name provided. Exiting...\n');
-    process.exit(0);
-  }
-  process.stdout.write(`Your name is: ${name}\r`);
-  readline.close();
-});
-
-readline.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
-})
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  })
+  .on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  })
