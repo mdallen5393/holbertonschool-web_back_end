66d8e3344d2643cb91215f77a1e92a5f9aeb7586
mdallen5393 <allen_matthew@outlook.com>
2023-07-03 13:38:59 -0500

    Initial commit for project Unittests in JS

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+function calculateNumber(a, b) {
+  // Rounds two numbers and returns the sum
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const calculateNumber = require('./0-calcul');
+
+describe('My Function', function() {
+  it('should round the numbers and return their sum', function() {
+    const result = calculateNumber(1.5, 2.3);
+    assert.equal(result, 4);
+  });
+
+  it('should work with 0 values', function() {
+    const result = calculateNumber(1, 4);
+    assert.equal(result, 5);
+  });
+
+  it('should work with 0 values', function() {
+    const result = calculateNumber(0, 0);
+    assert.equal(result, 0);
+  });
+
+  it('should work with negative values', function() {
+    const result = calculateNumber(-1, -32);
+    assert.equal(result, -33);
+  });
+
+  it('should work with very large values', function() {
+    const result = calculateNumber(Number.MAX_SAFE_INTEGER, 0.2);
+    assert.equal(result, Number.MAX_SAFE_INTEGER);
+  });
+});
