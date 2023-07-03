676945984b043b49a15202246fb57ced3f5b0623
mdallen5393 <allen_matthew@outlook.com>
2023-07-03 13:58:19 -0500

    Initial commit for project Unittests in JS

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -17,6 +17,16 @@ describe('My Function', function() {
     assert.equal(result, 4);
   });
 
+  it('should round first number', function() {
+    const result = calculateNumber(1.1, 2);
+    assert.equal(result, 3);
+  });
+
+  it('should round second number', function() {
+    const result = calculateNumber(1, 2.1);
+    assert.equal(result, 3);
+  });
+
   it('should work with integer values', function() {
     const result = calculateNumber(1, 4);
     assert.equal(result, 5);
