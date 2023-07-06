6ee31b881cb2763d2484b61073cf6a72306b6858
mdallen5393 <allen_matthew@outlook.com>
2023-07-05 23:25:10 -0500

    Task 9. Regex integration testing

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', function () {
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', function () {
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865/cart/12', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal(`Payment methods for cart 12`);
+      done();
+    });
+  });
+
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865/cart/test', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
